60b02782d4db997b3bedc6328bd04a2d8da04a2c
OP (oppenheimer) <21008429+Ompragash@users.noreply.github.com>
2024-10-30 00:16:44 +0530

    Update docker.go

--- docker.go
+++ docker.go
@@ -198,7 +198,7 @@ func commandInfo() *exec.Cmd {
 func commandBuild(build Build) *exec.Cmd {
 	args := []string{
 		"bud",
-		"--storage-driver", "fuse-overlayfs",
+		"--storage-driver", "vfs",
 		"--log-level=debug",
 		"-f", build.Dockerfile,
 	}
